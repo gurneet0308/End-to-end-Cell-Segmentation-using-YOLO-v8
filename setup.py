035d504e2d179c47c89a251c44ca1ed10984e0f9
gurneet0308 <gurneet2414@gmail.com>
2024-06-23 02:22:17 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CellSegmentation',
+    version='0.0.0',
+    author='Gurneet',
+    author_email='gurneet2414@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
